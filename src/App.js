63b649df3cf851590d63538f04ca264edb7f5848
Cuong Nguyen <cuonghn8@live.com>
2017-07-29 20:23:23 +0700

    refactor: Lift the state up
    Move ListBooks state up to parent (App)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBook from './SearchBook'
 import './App.css'
+import * as BooksAPI from './BooksAPI'
 
-const BooksApp = (props) => {
-	return (
-		<div className="app">
-			<Route exact path="/" component={ListBooks} />
-			<Route path="/search" component={SearchBook} />
-		</div>
-	)
+class BooksApp extends Component {
+	state = {
+		loaded: false,
+		shelves: {
+			currentlyReading: [],
+			wantToRead: [],
+			read: []
+		}
+	}
+
+	componentDidMount() {
+		BooksAPI.getAll()
+			.then(books => {
+				const currentShelves = this.state.shelves
+
+				books.forEach(book => currentShelves[book.shelf].push(book))
+
+				this.setState({ shelves: currentShelves, loaded: true })
+			})
+			.catch(err => {
+				this.setState({ loaded: true })
+			})
+	}
+
+	handleChangeShelf = (book, newShelf) => {
+		BooksAPI.update(book, newShelf)
+			.then(() => {
+				this.setState(prevState => {
+					const currentShelves = prevState.shelves
+					const updatedShelves = {
+						[book.shelf]: currentShelves[book.shelf].filter(b => b.title !== book.title),
+						[newShelf]: currentShelves[newShelf].concat(Object.assign({}, book, { shelf: newShelf }))
+					}
+
+					return { shelves: Object.assign({}, currentShelves, updatedShelves) }
+				})
+			})
+	}
+
+	render() {
+		return (
+			<div className="app">
+				<Route
+					exact
+					path="/"
+					render={() => <ListBooks {...this.state} onChangeShelf={this.handleChangeShelf} />}
+				/>
+				<Route path="/search" component={SearchBook} />
+			</div>
+		)
+	}
 }
 
 export default BooksApp
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,83 +1,43 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 import Loading from './Loading'
-import * as BooksAPI from './BooksAPI'
 
-class ListBooks extends Component {
-	state = {
-		loaded: false,
-		shelves: {
-			currentlyReading: [],
-			wantToRead: [],
-			read: []
-		}
-	}
+const ListBooks = ({ loaded, shelves, onChangeShelf }) => {
 
-	componentDidMount() {
-		BooksAPI.getAll()
-			.then(books => {
-				const currentShelves = this.state.shelves
+	const { currentlyReading, wantToRead, read } = shelves
 
-				books.forEach(book => currentShelves[book.shelf].push(book))
-
-				this.setState({ shelves: currentShelves, loaded: true })
-			})
-			.catch(err => {
-				this.setState({ loaded: true })
-			})
-	}
-
-	changeShelf = (book, newShelf) => {
-		BooksAPI.update(book, newShelf)
-			.then(() => {
-				this.setState(prevState => {
-					const currentShelves = prevState.shelves
-					const updatedShelves = {
-						[book.shelf]: currentShelves[book.shelf].filter(b => b.title !== book.title),
-						[newShelf]: currentShelves[newShelf].concat(Object.assign({}, book, { shelf: newShelf }))
-					}
-
-					return { shelves: Object.assign({}, currentShelves, updatedShelves) }
-				})
-			})
-	}
-
-	render() {
-		const { loaded, shelves: { currentlyReading, wantToRead, read } } = this.state
-
-		return (
-			<div className="list-books">
-				<div className="list-books-title">
-					<h1>MyReads</h1>
-				</div>
-				<div className="list-books-content">
-					{loaded ? (
-						<div>
-							<BookShelf
-								shelf="Currently Reading"
-								books={currentlyReading}
-								onChangeShelf={this.changeShelf}
-							/>
-							<BookShelf
-								shelf="Want to Read"
-								books={wantToRead}
-								onChangeShelf={this.changeShelf}
-							/>
-							<BookShelf
-								shelf="Read"
-								books={read}
-								onChangeShelf={this.changeShelf}
-							/>
-						</div>
-					) : <Loading />}
-				</div>
-				<div className="open-search">
-					<Link to='/search'>Add a book</Link>
-				</div>
+	return (
+		<div className="list-books">
+			<div className="list-books-title">
+				<h1>MyReads</h1>
 			</div>
-		)
-	}
+			<div className="list-books-content">
+				{loaded ? (
+					<div>
+						<BookShelf
+							shelf="Currently Reading"
+							books={currentlyReading}
+							onChangeShelf={onChangeShelf}
+						/>
+						<BookShelf
+							shelf="Want to Read"
+							books={wantToRead}
+							onChangeShelf={onChangeShelf}
+						/>
+						<BookShelf
+							shelf="Read"
+							books={read}
+							onChangeShelf={onChangeShelf}
+						/>
+					</div>
+				) : <Loading />}
+			</div>
+			<div className="open-search">
+				<Link to='/search'>Add a book</Link>
+			</div>
+		</div>
+	)
 }
 
 export default ListBooks
