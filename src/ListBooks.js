ccd17243b6ab75e44ae4e9a2acde31696bddd777
Cuong Nguyen <cuonghn8@live.com>
2017-07-29 17:44:35 +0700

    feat: Add Loading component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
+import Loading from './Loading'
 import * as BooksAPI from './BooksAPI'
 
 class ListBooks extends Component {
@@ -69,7 +70,7 @@ class ListBooks extends Component {
 								onChangeShelf={this.changeShelf}
 							/>
 						</div>
-					) : 'Loading'}
+					) : <Loading />}
 				</div>
 				<div className="open-search">
 					<Link to='/search'>Add a book</Link>
---
+++ src/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+	return (
+		<div className="loading">
+			<i className="fa fa-2x fa-spinner fa-pulse" aria-hidden="true"></i>
+		</div>
+	)
+}
+
+export default Loading
