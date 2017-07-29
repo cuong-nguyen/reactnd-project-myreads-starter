1302d3e6b327f97b547115c8e6b999f78cddd732
Cuong Nguyen <cuonghn8@live.com>
2017-07-29 23:04:07 +0700

    feat: Add sort-by

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import BookShelf from './BookShelf'
 import Loading from './Loading'
+import sortBy from 'sort-by'
+
+const filterAndSortBooks = (books, shelf, field = 'title') => {
+	const filteredBooks = books.filter(book => book.shelf === shelf)
+	return filteredBooks.sort(sortBy(field))
+}
 
 const ListBooks = ({ loaded, books, onChangeShelf }) => {
 	return (
@@ -15,17 +21,17 @@ const ListBooks = ({ loaded, books, onChangeShelf }) => {
 					<div>
 						<BookShelf
 							shelf="Currently Reading"
-							books={books.filter(book => book.shelf === 'currentlyReading')}
+							books={filterAndSortBooks(books, 'currentlyReading')}
 							onChangeShelf={onChangeShelf}
 						/>
 						<BookShelf
 							shelf="Want to Read"
-							books={books.filter(book => book.shelf === 'wantToRead')}
+							books={filterAndSortBooks(books, 'wantToRead')}
 							onChangeShelf={onChangeShelf}
 						/>
 						<BookShelf
 							shelf="Read"
-							books={books.filter(book => book.shelf === 'read')}
+							books={filterAndSortBooks(books, 'read')}
 							onChangeShelf={onChangeShelf}
 						/>
 					</div>
