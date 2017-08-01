e892eb6059dff2a429a09ffd1e87e57101a5f99f
Cuong Nguyen <cuonghn8@live.com>
2017-08-01 21:15:14 +0700

    fix: Set correct state after changing Book shelf in Search

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const BookShelfChanger = ({ books, onChangeShelf }) => {
-	const { length: noOfBooks } = books
-	return (
-		<div className="book-shelf-changer">
-			<select
-				value={books[0].shelf}
-				onChange={({ target }) => onChangeShelf(books, target.options[target.selectedIndex].value)}
-			>
-				<option value="none" disabled>{noOfBooks === 1 ? 'Move to...' : 'Move all to...'}</option>
-				<option value="currentlyReading">Currently Reading</option>
-				<option value="wantToRead">Want to Read</option>
-				<option value="read">Read</option>
-				<option value="none">None</option>
-			</select>
-		</div>
-	)
+class BookShelfChanger extends Component {
+	state = {
+		selectedShelf: this.props.books[0].shelf,
+		books: this.props.books
+	}
+
+	handleOnShelfChange = ({ target }) => {
+		const selectedShelf = target.options[target.selectedIndex].value
+		this.props.onChangeShelf(this.state.books, selectedShelf)
+		this.setState({ selectedShelf })
+	}
+
+	render() {
+		const { length: noOfBooks } = this.props.books
+		const { selectedShelf } = this.state
+
+		return (
+			<div className="book-shelf-changer">
+				<select
+					value={selectedShelf}
+					onChange={this.handleOnShelfChange}
+				>
+					<option value="none" disabled>{noOfBooks === 1 ? 'Move to...' : 'Move all to...'}</option>
+					<option value="currentlyReading">Currently Reading</option>
+					<option value="wantToRead">Want to Read</option>
+					<option value="read">Read</option>
+					<option value="none">None</option>
+				</select>
+			</div>
+		)
+	}
 }
 
 BookShelfChanger.propTypes = {
